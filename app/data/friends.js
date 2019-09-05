e1d474926e24940838dfe696c2c0715f697e2a41
evandavies11 <evandavies11@gmail.com>
2019-09-05 14:51:45 -0500

    most html before i screw it up

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,13 +4,23 @@
 
 var friendArray = [
     {
-        friendName: "Ahmed",
-        friendPhoto: "ahmed@example.com",
+        friendName: "jimmybobby",
+        friendPhoto: "jimmybobby@example.com",
         scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
 
         ]
     }
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = tableArray;
+module.exports = friendArray;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
-var path = require("path");
+
 
 var app = express();
-var app = path();
+
 
 // Sets an initial port. We"ll use this later in our listener
 
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //sets the routes 
-require("app/routing/apiRoutes.js")(app);
-require("app/routing/htmlRoutes")(app);
+//require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 //listener port
 app.listen(PORT, function () {
